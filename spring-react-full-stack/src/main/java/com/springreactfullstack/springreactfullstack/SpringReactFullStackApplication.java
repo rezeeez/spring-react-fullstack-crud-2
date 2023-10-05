0af54c6d3f4d58b6f1781e21c77990015d8c0cf4
Reze <seanjerico.1999@gmail.com>
2023-10-05 16:49:13 +0800

    spring

---
+++ spring-react-full-stack/src/main/java/com/springreactfullstack/springreactfullstack/SpringReactFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.springreactfullstack.springreactfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactFullStackApplication.class, args);
+	}
+
+}
---
+++ spring-react-full-stack/src/test/java/com/springreactfullstack/springreactfullstack/SpringReactFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreactfullstack.springreactfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
