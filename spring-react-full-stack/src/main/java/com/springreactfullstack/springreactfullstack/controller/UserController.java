6bdc5d485f0156dc011bb2d7cddafc8039c30af3
Reze <seanjerico.1999@gmail.com>
2023-10-06 22:56:02 +0800

    finalized front end

--- spring-react-full-stack/src/main/java/com/springreactfullstack/springreactfullstack/controller/UserController.java
+++ spring-react-full-stack/src/main/java/com/springreactfullstack/springreactfullstack/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.springreactfullstack.springreactfullstack.controller;
 
+import com.springreactfullstack.springreactfullstack.exception.UserNotFoundException;
 import com.springreactfullstack.springreactfullstack.model.User;
 import com.springreactfullstack.springreactfullstack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,28 @@ public class UserController {
     List<User> getAllUsers(){ //method for getting all users
         return userRepository.findAll(); //JpaRep method
     }
+    @GetMapping("/users/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()-> new UserNotFoundException(id)); // exception error
+    }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user->{
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
 
+    }
+    @DeleteMapping("/user/{id}") //delete mapping
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User id "+id+ " is deleted.";
+    }
 }
